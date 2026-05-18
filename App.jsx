89ff3326ace91e6941467147a01fa0f337cf1bd8
alaabenmahmoud629-rgb <alaabenmahmoud629@gmail.com>
2026-05-18 12:40:38 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -18,7 +18,7 @@ import {
   Globe2,
 } from "lucide-react";
 
-const profilePhotoUrl = "/alaa-profile.PNG";
+const profilePhotoUrl = "/alaa-profile .png";
 const linkedInUrl = "https://www.linkedin.com/in/ben-mahmoud-alla-eddine";
 const email = "alaabenmahmoud629@gmail.com";
 const phone = "+216 98 606 395";
